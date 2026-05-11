da1fa2e5abba9a483f3964d96d526f2e2a81ca2e
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-11 12:52:29 +1000

    [enhance]

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
-import { GLOBAL } from "config"
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import { Header } from "component/shared/header"
 
 function App() {
+  const [currScreen, setCurrScreen] = useState('dashboard')
   return (
     <div className={"App"}>
-      <Header title={GLOBAL.APP_NAME} />
+      <Header currScreen={currScreen}onNavigate={setCurrScreen} />
+      {/* {currScreen === 'dashboard' && <Dashboard/>} */}
       <Outlet />
     </div>
   )
