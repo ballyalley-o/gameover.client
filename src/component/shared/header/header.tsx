c99ce5a779031863b3188bd640c3e26710bc1608
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-11 12:48:20 +1000

    [enhance]: updated layout

--- src/component/shared/header/header.tsx
+++ src/component/shared/header/header.tsx
@@ -1,23 +1,44 @@
+import { NAV_DIR } from 'config'
 import { ASSET_DIR } from 'config/dir.config'
 import { Container } from 'design/primitive'
-import { formatText } from 'utility'
 
-const Header = ({ title }: { title: string }) => {
+const Header = ({ currScreen, onNavigate }: { currScreen: string, onNavigate: (screen: string) => void } ) => {
   return (
-    <div className={"z-40 sticky top-0 bg-background"}>
-      <header className={'header'}>
+    <nav className={"z-40 sticky bg-background"}>
         <Container>
-          <div className={"cluster"}>
-            <div className={'inline'}>
-              <a href={"/"} className={"inline"}>
-                <img alt="logo" src={ASSET_DIR.LOGO} className={'h-12 w-12'} />
-              </a>
-              <span className={'hidden lg:block text-xl'}>{formatText(title, 'capitalize')}</span>
-            </div>
+          <div className={"cluster h-16"}>
+              <div className={'inline'}>
+                <a href={"/"} className={"inline"}>
+                  <img alt="logo" src={ASSET_DIR.NAV} className={'h-8'} />
+                </a>
+              </div>
+              <div className={'nav-wrapper'}>
+                <div className={'nav-menu'}>
+                  {NAV_DIR.HEADER.map((_i) => {
+                    const isActive = currScreen === _i.id;
+                    return (
+                      <button
+                        key={_i.id}
+                        onClick={() => onNavigate(_i.id)}
+                        style={{ background: isActive ?  `var(--color-palette-brand)` : `var(--bg-transparent)` }}
+                        className={`button h-full tracking-wide transition-colors cursor-pointer
+                          ${isActive
+                            ? "text-black"
+                            : "text-muted-foreground hover:text-foreground hover:bg-white/5"
+                        }`}>
+                        {_i.label}
+                      </button>
+                    )})}
+                </div>
+                <div className="league-menu">
+                  <span>The Baller League</span>
+                  <div className="league-avatar" />
+                </div>
+              </div>
           </div>
         </Container>
-      </header>
-    </div>
+
+    </nav>
   )
 }
 
