7956466115371c6018b87b0311dee110a8a875c3
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-11 12:46:27 +1000

    [config]: added new asset in asset dir

--- src/config/dir.config.ts
+++ src/config/dir.config.ts
@@ -1,5 +1,6 @@
 import { combine } from "utility"
 
 export const ASSET_DIR = {
-    LOGO: combine('asset', 'gameover-alt-2.png')
+  LOGO: combine("asset", "gameover-alt-2.png"),
+  NAV : combine("asset", "gameover-wordmark.png"),
 }
