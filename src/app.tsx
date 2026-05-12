24300d2a1d5130f6ab490f507cfd6370820bd4e2
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-12 14:25:16 +1000

    [enhance]: dashboard wired up

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import { Header } from "component/shared/header"
+import { Dashboard } from "page/dashboard/dashboard"
 
 function App() {
   const [currScreen, setCurrScreen] = useState('dashboard')
   return (
     <div className={"App"}>
       <Header currScreen={currScreen}onNavigate={setCurrScreen} />
-      {/* {currScreen === 'dashboard' && <Dashboard/>} */}
+      {currScreen === 'dashboard' && <Dashboard />}
       <Outlet />
     </div>
   )
