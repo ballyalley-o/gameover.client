312fb584094ca2a3a63736d20d76a689c0fc82ee
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-12 14:23:08 +1000

    [enhance]: nav items localizations

--- src/config/nav.config.ts
+++ src/config/nav.config.ts
@@ -1,9 +1,48 @@
+import type { LocaleKey } from "utility"
+
 const _HEADER_DASHBOARD = [
-    { id: 'dashboard', label: 'Dashboard' },
-    { id: 'draft-room', label: 'Draft Room' },
-    { id: 'player-stat', label: 'Player Stats' }
-]
+    { id: 'dashboard', labelKey: 'nav.dashboard' },
+    { id: 'draft-room', labelKey: 'nav.draft_room' },
+    { id: 'player-stat', labelKey: 'nav.player_stats' }
+] satisfies {
+  id      : string
+  labelKey: LocaleKey
+}[]
+
+const _SIDEBAR_DEFAULT = [
+    {
+        labelKey: 'nav.sidebar.league',
+        items: [
+            { id: 'standings', labelKey: 'nav.sidebar.items.standings' },
+            { id: 'playoffs', labelKey: 'nav.sidebar.items.playoffs' },
+            { id: 'calendar', labelKey: 'nav.sidebar.items.calendar' },
+            { id: 'transactions', labelKey: 'nav.sidebar.items.transactions' },
+            { id: 'power_rankings', labelKey: 'nav.sidebar.items.power_rankings' },
+            { id: 'news_feed', labelKey: 'nav.sidebar.items.news_feed' },
+        ],
+    },
+    {
+        labelKey: 'nav.sidebar.team',
+        items: [
+            { id: 'roster', labelKey: 'nav.sidebar.items.roster' },
+            { id: 'schedule', labelKey: 'nav.sidebar.items.schedule' },
+        ]
+    },
+    {
+        labelKey: 'nav.sidebar.player',
+        items: [
+            { id: 'stats', labelKey: 'nav.sidebar.items.stats' }
+        ]
+    }
+] satisfies {
+  labelKey: LocaleKey
+  items: {
+    id      : string
+    labelKey: LocaleKey
+  }[]
+}[]
 
 export const NAV_DIR = {
-  HEADER: _HEADER_DASHBOARD
+  HEADER : _HEADER_DASHBOARD,
+  SIDEBAR: _SIDEBAR_DEFAULT
 }
