26d593bcfd279669cf063c3384c9577114d816e1
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-14 02:14:09 +1000

    feat: add NavLabelType definition to global typings

--- src/typing/global.d.ts
+++ src/typing/global.d.ts
@@ -1,8 +1,16 @@
 import { en, fr, es } from "locale"
+import { LocaleKey } from "utility"
 
 declare global {
     declare module "*.css"
     declare type AppLocale = typeof en | typeof fr | typeof es
 
     declare type AppValueScaleType = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+
+    declare type NavLabelType =  {
+      id       : string
+      labelKey : LocaleKey
+      to       : string
+      icon    ?: React.ReactNode
+    }
 }
