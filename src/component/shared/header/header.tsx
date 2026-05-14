85adcb4a9a0bdfc1a366051e1bc263e0c1078b1d
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-15 00:30:18 +1000

    [refactor]

--- src/component/shared/header/header.tsx
+++ src/component/shared/header/header.tsx
@@ -1,27 +1,23 @@
 import { NAV_DIR } from 'config'
-import { ASSET_DIR } from 'config/dir.config'
 import { Container } from 'design/primitive'
 import { NavHeaderButton } from 'component/shared'
+import { BrandNavLogo } from 'component/shared/brand'
 
-const Header = ({ currScreen }: { currScreen: string, onNavigate: (screen: string) => void } ) => {
+const Header = () => {
   return (
     <nav className={"z-40 sticky bg-background border-b-2 border-gray-400/10"}>
         <Container>
           <div className={"cluster h-16"}>
-              <div className={'inline'}>
-                <a href={"/"} className={"inline"}>
-                  <img alt="logo" src={ASSET_DIR.NAV} className={'h-8'} />
-                </a>
-              </div>
+            <BrandNavLogo />
               <div className={'nav-wrapper'}>
                 <div className={'nav-menu'}>
                   {NAV_DIR.HEADER.map((_i) => {
-                    const isActive = currScreen === _i.id;
                     return (
-                      <NavHeaderButton item={_i} isActive={isActive} />
+                      <NavHeaderButton key={_i.id} item={_i} />
                     )})}
                 </div>
                 <div className={"league-menu"}>
+                  {/* user component */}
                   <span>The Baller League</span>
                   <div className={"league-avatar"} />
                 </div>
