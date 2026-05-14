4d9f95c2d3932557e49b225aa51a799c192e8c20
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-15 00:50:36 +1000

    [layout]

--- src/page/dashboard/dashboard.tsx
+++ src/page/dashboard/dashboard.tsx
@@ -1,21 +1,7 @@
-import { Sidebar } from "component/shared/sidebar"
-import { NAV_DIR } from "config"
-import { transl } from "utility"
-
 function DashboardPage() {
 
   return (
-    <div className={'flex min-h-[calc(100vh-60px)]'}>
-      <aside  className={"text-md w-56 border-r border-gray-400/10  shrink-0 py-3 flex flex-col gap-1 pl-5 pr-2"} style={{ background: "var(--sidebar)" }}>
-        <div className={"px-4 py-1 mb-1 space-y-2"}>
-          <div>{transl('nav.sidebar.league_lounge')}</div>
-        </div>
-        <div className={"border-t border-border border-gray-400/10 mb-1"} />
-        {NAV_DIR.SIDEBAR.map((_s) => (
-          <Sidebar key={_s.labelKey} labelKey={_s.labelKey} items={_s.items}/>
-        ))}
-      </aside>
-    </div>
+    <div>Dashboard</div>
   )
 }
 
