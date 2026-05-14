414171017b9a999986767a0b8582df47f016babd
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-05-15 00:50:49 +1000

    [feat]: add TeamPage route and ensure consistent loading of pages

--- src/route/element.tsx
+++ src/route/element.tsx
@@ -3,3 +3,4 @@ import { Loadable } from "route/loadable"
 
 export const DashboardPage = Loadable(lazy(() => import("page/dashboard/dashboard")))
 export const LeaguePage = Loadable(lazy(() => import('page/league/league')))
+export const TeamPage = Loadable(lazy(() => import('page/team/league')))
